8783b57f31f9e6dd49a083782b90b15f4235a002
Ntombi97 <nontombishabangu@gmail.com>
2024-04-02 11:21:32 +0200

    updates

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-StrNumber = str(number)
-LastDigit = StrNumber[-1]
-LastDigitNumber = int(LastDigit)
-if LastDigitNumber > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, LastDigitNumber))
-elif LastDigitNumber == 0:
-        print("Last digit of {} is {} and is 0".format(number, LastDigitNumber))
-elif LastDigitNumber < 6:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, LastDigitNumber))
+if number < 0:
+    LastDigit = number % -10
+else:
+    LastDigit = number % 10
+if LastDigit > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, LastDigit))
+elif LastDigit == 0:
+        print("Last digit of {} is {} and is 0".format(number, LastDigit))
+elif LastDigit < 6:
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, LastDigit))
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for Numbers in range(00, 99):
+    print("{:02d}, ".format(Numbers), end = "")
+print("99")
